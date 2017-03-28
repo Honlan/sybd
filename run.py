b47bc4c5c8e0cae22342686cf0455fed8c796728
Honlan <honlan@HonlandeMacBook-Air.local>
2017-03-28 11:31:19 +0800

    complete human flow in realtime

--- run.py
+++ run.py
@@ -34,7 +34,52 @@ def closedb(db,cursor):
 # 首页
 @app.route('/')
 def index():
-	return render_template('index.html')
+	dataset = {}
+
+	(db,cursor) = connectdb()
+
+	cursor.execute("select * from json_data where page=%s", ['index'])
+	
+	json_data = cursor.fetchall()
+	tmp = {}
+	for item in json_data:
+		tmp[item['keyword']] = json.loads(item['json'])
+	dataset['json'] = tmp
+
+	closedb(db,cursor)
+
+	hour = str(((int(time.time()) % (3600 * 24)) / 3600 + 8) % 24)
+	dataset['json']['human']['hour'] = hour
+	timestamp = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+	for key in dataset['json']['human']['bar'].keys():
+		dataset['json']['human']['bar'][key] = [int(dataset['json']['human']['bar'][key][t][0] / float(dataset['json']['human']['bar'][key][t][1])) for t in timestamp]
+
+	for key in dataset['json']['human']['tree'].keys():
+		for p in dataset['json']['human']['tree'][key].keys():
+			for sp in dataset['json']['human']['tree'][key][p].keys():
+				dataset['json']['human']['tree'][key][p][sp] = int(dataset['json']['human']['tree'][key][p][sp][0] / dataset['json']['human']['tree'][key][p][sp][1])
+
+	for t in dataset['json']['human']['tree'].keys():
+		tmp = []
+		for p in dataset['json']['human']['tree'][t].keys():
+			pvalue = 0
+			children = []
+			for sp in dataset['json']['human']['tree'][t][p].keys():
+				children.append({
+					'name': sp,
+					'path': p + '/' + sp,
+					'value': dataset['json']['human']['tree'][t][p][sp]
+					})
+				pvalue += dataset['json']['human']['tree'][t][p][sp]
+			tmp.append({
+				'name': p,
+				'path': p,
+				'value': pvalue,
+				'children': children
+				})
+		dataset['json']['human']['tree'][t] = tmp
+	
+	return render_template('index.html', dataset=json.dumps(dataset))
 
 # 宏观
 @app.route('/macro')
